98c3ae101bc21a644d72728ce60247be53d83376
MEScherer <schererm@stanford.edu>
2026-06-19 22:13:14 -0700

    Finished the induced energy function

--- build/main.py
+++ build/main.py
@@ -7,16 +7,43 @@ from rdkit import Chem
 from rdkit.Chem import rdDetermineBonds
 import easyxtb
 
-def get_induced_energy(geometry, fragments, alpha=1):
-    """Gets the induced energy for the fragment
+def get_induced_energy(geometry, fragments, gamma=146.44, p=6, sigma=1.0061, R_0=3.8164):
+    """Gets the induced energy for a two-fragment AFIR system
     Args: 
         geometry: easyxtb geometry | the total geometry
         fragments: list{RDKit Mol} | a list of the fragments in this geometry
-        alpha: int | tunable force parameter | default = 1
+        gamma: float | collision energy parameter (kJ/mol) | default = 146.44 kJ/mol (35 kcal/mol)
+        p: int | default = 6
+        sigma: float | default = 1.0061 kJ/mol
+        R_0: float | default = 3.8164 Angstrom
     Returns:
         induced_force: int | induced force
     """
-    return None
+    # make the alpha parameter
+    alpha = gamma/(R_0*(2**(-1/p)-(1+(1+gamma/sigma)**(0.5))**(-1/p)))
+    
+    # prepare the position and radii matrices
+    pos_1 = get_frag_positions(fragments[0], geometry)
+    pos_2 = get_frag_positions(fragments[1], geometry)
+    rad_1 = get_frag_radii(fragments[0])
+    rad_2 = get_frag_radii(fragments[1])
+
+    # calculate distance matrix (r_ij)
+    diff = pos_1[:, np.newaxis, :] - pos_2[np.newaxis, :, :] # shape (N,M,3)
+    r_ij = np.linalg.norm(diff, axis=2) # shape (N, M)
+    
+    # calculate radii matrix (R_ij)
+    R_ij_sum = rad_1[:, np.newaxis] + rad_2[np.newaxis, :]
+
+    # compute numerator and denominator (small epsilon to handle division by zero)
+    epsilon = 1e-10
+    scaling_matrix = (R_ij_sum/(r_ij+epsilon))**p
+    num = np.sum(scaling_matrix * r_ij)
+    denom = np.sum(scaling_matrix)
+
+    # calculate induced energy
+    induced_energy = alpha*num/denom
+    return induced_energy
 
 def generate_starting_confs(geometry):
     """Rotates the entire molecule in 3D space.
@@ -44,7 +71,9 @@ def get_frag_radii(fragment):
     'S': 1.80,
     'Cl': 1.75
     }
-    return None
+    radii_pre = [VDW_RADII[atom.GetSymbol()] for atom in fragment.GetAtoms()]
+    radii = np.array(radii_pre)
+    return radii
 
 def get_frag_positions(fragment, geometry):
     """Returns a matrix of atomic positions corresponding to a specific fragment
@@ -197,6 +226,19 @@ def test_smiles_methanol(save_path):
     print(f"Returned {result}")
     return False
 
+# TEST
+def test_get_frag_radii_methanol(save_path):
+    """Testing get_frag_radii function on methanol molecule.
+    Result: passed
+    """
+    input_geom = easyxtb.Geometry.load_file(save_path / "molecules" / "methanol.xyz")
+    geom = easyxtb.calculate.optimize(input_geom, level="normal")
+    mol = get_fragments(geom)[0] # should return a single fragment
+
+    # test
+    expected = np.array([1.7, 1.52, 1.2, 1.2, 1.2, 1.2])
+    return np.array_equal(get_frag_radii(mol), expected)
+
 if __name__ == "__main__":
     # establish directory and save paths
     script_dir = Path(__file__).resolve().parent
@@ -205,6 +247,7 @@ if __name__ == "__main__":
 
     # Tests
     print("Test Results:")
+    print(f"   test_get_frag_radii_methanol: {test_get_frag_radii_methanol(save_path)}")
     print(f"   test_get_frag_positions_methanol: {test_get_frag_positions_methanol(save_path)}")
     print(f"   test_smiles_methanol: {test_smiles_methanol(save_path)}")
     print(f"   test_get_fragments_methanol: {test_get_fragments_methanol(save_path)}")
